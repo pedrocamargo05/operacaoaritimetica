7c7345775338b5827146020b8fd71289a2f05106
Pedro DROPE <160151941+pedrocamargo05@users.noreply.github.com>
2024-06-14 12:43:42 -0300

    carga inicial

---
+++ operacaoaritimetica/script.js
@@ -0,0 +1,29 @@
+ /* var a = 10;
+  var b = 15;
+   
+  var c = a + b;
+
+  console.log (c);
+*/
+/*
+  var a = 10;
+  var b = 15;
+   
+  var c = a - b;
+
+  console.log (c);
+  */
+/*
+  var a = 10;
+  var b = 15;
+   
+  var c = a * b;
+
+  console.log (c);
+*/
+  var a = 10;
+  var b = 15;
+   
+  var c = a / b;
+
+  console.log (c);
